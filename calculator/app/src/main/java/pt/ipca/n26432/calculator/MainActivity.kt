34a5821d3265484f3e18241b97e1344ca07437d4
Daniel Pereira <danielalexis.o.pereira@gmail.com>
2024-12-16 14:14:51 +0000

    Update MainActivity.kt

--- calculator/app/src/main/java/pt/ipca/n26432/calculator/MainActivity.kt
+++ calculator/app/src/main/java/pt/ipca/n26432/calculator/MainActivity.kt
@@ -49,7 +49,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-// Screen
 @Composable
 fun CalculatorScreen(modifier: Modifier = Modifier) {
     var display by remember { mutableStateOf("0") }
